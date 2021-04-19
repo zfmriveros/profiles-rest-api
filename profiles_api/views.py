9c1e5ec8407f03ec62e5fd4fdd952254ca5061d6
mriveros <mriveros@zerofo.com>
2021-04-19 12:01:00 -0400

    Added viewset to profil_api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
+from rest_framework import viewsets
 
 
 class HelloApiView(APIView):
@@ -38,3 +39,74 @@ class HelloApiView(APIView):
         return Response({'method': 'DELETE'})
 
 
+class HelloViewSet(viewsets.ViewSet):
+    """
+    Test ApiViewSet
+    """
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """
+        Return a hello message
+        :param request:
+        :return:
+        """
+        a_viewset = [
+            'Users actions (list, create, retrieve, update, patial_update)',
+            "Automatically maps URL using routers"
+            'provide more functionality with list code'
+        ]
+        return Response({'message': 'Hello', 'view_set': a_viewset})
+
+    def create(self, request):
+        """
+        Create a new hello message
+        :param request:
+        :return:
+        """
+        serializers = self.serializer_class(data=request.data)
+        if serializers.is_valid():
+            name = serializers.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializers.errors,
+                status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """
+        Handle getting an object by its ID
+        :param request:
+        :param ph:
+        :return:
+        """
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """
+        Updating object
+        :param request:
+        :param ph:
+        :return:
+        """
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """
+        Updating object partially
+        :param request:
+        :param ph:
+        :return:
+        """
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """
+        Remove object
+        :param request:
+        :param ph:
+        :return:
+        """
+        return Response({'http_method': 'DELETE'})
